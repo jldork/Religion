4d5797e18cf84f122cc0e3554dbb5c6f6f806e3d
James Leung <jamesleung417@gmail.com>
2017-07-24 20:40:21 -0400

    Pulled out classes

---
+++ book/book.py
@@ -0,0 +1,68 @@
+import re
+import spacy
+import pandas as pd
+from functools import reduce
+from itertools import combinations
+from collections import Counter, defaultdict
+
+from stop_words import get_stop_words
+from splitter.functions import *
+
+stop_words = get_stop_words('en')
+nlp = spacy.load('en')
+
+class Line:
+    def __init__(self, text):
+        self.text =  text
+        self.nouns = self.get_nouns(text)
+
+    def get_nouns(self, line):
+        doc = nlp(line)
+        nouns = [ word.text for word in doc if word.pos_ in ['NOUN', 'PROPN'] ]
+        return Counter(nouns)
+
+class Book:
+    def __init__(self, text):
+        self.text = self.clean_book(text)
+        lines = re.split(r'[0-9]+:[0-9]+ ', self.text)
+        self.title = lines[0]
+        self.lines = lines[1:]
+        self.nouns = [Line(line) for line in self.lines]
+        self.counts = self.get_counts(self.nouns)
+        self.edges = self.get_edge_frame_from_book()
+    
+    def clean_book(self, book):
+        cleaned_book = clean_text(book.replace('\ufeff','').replace('\n',' '), spaces=True)
+        pattern = re.compile(r'\b(' + r'|'.join(stop_words) + r')\b\s*')
+        return pattern.sub('', cleaned_book.lower())
+
+    def get_counts(self, lines, ctt_filter=25):
+        noun_list = [line.nouns for line in lines]
+        nouns = reduce(lambda x, y: { k: x.get(k, 0) + y.get(k, 0) for k in set(x) | set(y) },noun_list)
+
+        df = pd.Series(nouns).sort_values(ascending=False).to_frame()
+        df.columns = ['count']
+        df = df[df['count'] > ctt_filter]
+        return df
+
+    def get_filtered_combinations(self, nouns):
+        combos = []
+        for combo in combinations(nouns,2):
+
+            common_char = all([node in self.counts.index for node in combo])
+
+            if common_char:
+                combos.append(combo)
+
+        return combos
+
+    def get_edge_frame_from_book(self):
+        tot_edges = pd.DataFrame()
+
+        for line in self.nouns: 
+            edges = pd.DataFrame(self.get_filtered_combinations(line.nouns)) 
+            tot_edges = tot_edges.append(edges)
+
+        tot_edges.columns = ['node_1','node_2']
+        tot_edges = tot_edges.reset_index().drop('index',axis=1)
+        return tot_edges
---
+++ graph/network.py
@@ -0,0 +1,57 @@
+import networkx as nx
+import community
+from networkx.drawing.layout import fruchterman_reingold_layout
+from functools import reduce
+
+class NetworkFrame:
+
+    def __init__(self, df, node_columns):
+        self.df = df
+        self.node_columns = node_columns
+        self.nodes = self.get_nodes()
+        self.edges = self.get_edges()
+        self.graph = self.get_graph()
+        self.betweenness = nx.betweenness_centrality_source(
+            self.graph, normalized=True,
+            weight='weight', sources=self.nodes
+        )
+
+    def get_nodes(self):
+        list_of_nodes = [list(self.df[col].unique()) for col in self.node_columns]
+        return set(reduce(lambda x, y: x + y, list_of_nodes))
+
+    def get_edges(self):
+        edges = []
+        for i, record in self.df.iterrows():
+            edge = [record[col] for col in self.node_columns]
+            edges.append((edge[0], edge[1]))
+        return edges
+
+    def get_graph(self):
+        G = nx.Graph()
+        G.add_nodes_from(self.nodes)
+        G.add_edges_from(self.edges)
+        return G
+
+    def draw_graph(self, colors=None):
+        pos = fruchterman_reingold_layout(self.graph)
+        node_sizes = [ 100000 * x * x + 50 
+            for x in list(self.betweenness.values())]
+
+        if not colors:
+            parts = community.best_partition(self.graph)
+            colors = list(parts.values())
+
+        nx.draw_networkx_nodes(
+            self.graph, pos, node_size=node_sizes, node_color=colors,
+            alpha=0.5, line_color=None
+        )
+        nx.draw_networkx_edges(
+            self.graph, pos,
+            alpha=0.05, style='solid'
+        )
+        nx.draw_networkx_labels(
+            self.graph, pos,
+            {node: node for node in self.nodes},
+            font_size=8
+        )
